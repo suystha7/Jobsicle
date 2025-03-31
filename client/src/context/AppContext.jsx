57624db9deda554b0053a68d46af041aa9c6fccf
Anisha Nayaju <anishanayaju26@gmail.com>
2025-03-31 15:53:20 +0545

    App context

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,11 @@
+import React, { createContext } from "react";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = (props) => {
+  const value = {};
+
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
