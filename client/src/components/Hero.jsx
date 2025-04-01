5c98b487baa7bd4c3ab4e1d0e506266757b9bb29
Anisha Nayaju <anishanayaju26@gmail.com>
2025-04-01 21:49:20 +0545

    Hero section

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Hero = () => {
+  return (
+    <div className="container 2xl:px-20 mx-auto my-10">
+      <div className="bg-gradient-to-r from-purple-800 to-purple-950 text-white py-16 text-center mx-2 rounded-xl">
+        <h1>Over 10,000+ jobs to apply</h1>
+        <p>
+          Your Next Big Career Move starts Right here - Explore the Best Job
+          here
+        </p>
+        <div>
+          <div>
+            <img src={assets.search_icon} alt="" />
+            <input
+              type="text"
+              placeholder="search for jobs"
+              className="max-sm:text-xs p-2 rounded outline-none w-full"
+            />
+          </div>
+          <div>
+            <img src={assets.location_icon} alt="" />
+            <input
+              type="text"
+              placeholder="search for location"
+              className="max-sm:text-xs p-2 rounded outline-none w-full"
+            />
+          </div>
+          <button>Search</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
